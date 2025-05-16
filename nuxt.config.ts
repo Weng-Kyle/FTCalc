40a69d353f4dacfa1d5d0c76f59f300765ea1ebd
weng-kyle <weng.kyle@outlook.com>
2025-05-16 14:19:32 -0600

    Initial Commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { presetDaisy } from '@ameinhardt/unocss-preset-daisy';
+import { defineNuxtConfig } from 'nuxt/config';
+import { presetUno } from 'unocss';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  css: ['@unocss/reset/tailwind.css'],
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/image']
+  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/image', '@unocss/nuxt'],
+  unocss: {
+    presets: [presetUno(), presetDaisy()]
+  },
 })
